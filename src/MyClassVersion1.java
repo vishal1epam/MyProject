29e3205fc5948eb941e0c8db3b961b6a9b4fcae5
vishal1epam <86233570+vishal1epam@users.noreply.github.com>
2021-06-22 07:15:26 +0530

    Initial commit

--- src/MyClassVersion1.java
+++ src/MyClassVersion1.java
@@ -1,5 +1,9 @@
 public class MyClassVersion1 {
     public static void main(String [] args){
         System.out.println("Hello! welcome to Version1!");
+<<<<<<< HEAD
+=======
+        System.out.println("Added one more SOP");
+>>>>>>> 3898ffe (Initial commit)
     }
 }
